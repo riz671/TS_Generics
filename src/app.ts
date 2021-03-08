97048b464a87cd371a8d478ac2d145213dde84ed
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2021-03-08 10:18:34 -0800

    use generic utility functions

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ function merge<T extends object, U extends object>(obj1: T, obj2: U) {
 }
 
 // without T and U. we don't really know what's inside output object.
-// Generics makes it really simple to object consists something
+// Generics makes it really simple to object consists of something
 let output1 = merge({ name: "Rick Wayne" }, { age: 23, gender: "Female" });
 // Though output 2 has different values than output 1, with generics
 // TS can infer that favFood prop exists within output 2
@@ -59,10 +59,13 @@ let output = extractAndConvert({ name: "Gianna" }, "name");
 console.log(output);
 */
 
+/*
 // ====================================
 // GENERIC CLASSES
 // ====================================
 
+// By adding generic of T
+// I can instantiate this class such that T can be any type. (Look below at examples)
 class DataStorage<T> {
   private data: T[] = [];
 
@@ -75,6 +78,48 @@ class DataStorage<T> {
   }
 
   getItem() {
-    return [...this.data];
+    return this.data;
   }
 }
+
+// Since I added a T generic
+// T can be number, string, boolean, etc.
+const stringStore = new DataStorage<string>();
+stringStore.addItem("Pokemon");
+stringStore.addItem("Dracula");
+stringStore.removeItem("Pokemon");
+console.log(stringStore.getItem());
+
+const numberStore = new DataStorage<number>();
+numberStore.addItem(10);
+numberStore.addItem(14);
+numberStore.removeItem(10);
+console.log(numberStore.getItem());
+*/
+
+/*
+// ====================================
+// GENERIC UTILITY EXAMPLES
+// ====================================
+
+interface CourseGoals {
+  courses: string[];
+  dateCompleted: Date;
+  rating: number;
+}
+
+// Partial keywords makes forced props from interface optional
+// Then you can input the changes like so below
+// return as CourseGoals for extra type safety
+function createCourseGoal(
+  courses: string[],
+  date: Date,
+  rating: number
+): CourseGoals {
+  let goalsObj: Partial<CourseGoals> = {}; // Partial Keyword HERE
+  goalsObj.courses = courses;
+  goalsObj.dateCompleted = date;
+  goalsObj.rating = rating;
+  return goalsObj as CourseGoals;
+}
+*/
